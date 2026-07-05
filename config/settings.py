17767bfea9e74f3ae94e8d7819c7679b84b961e1
Deploy <deploy@pdf-to-audio.local>
2026-07-05 22:03:05 +0530

    Switch to STORAGES setting for staticfiles

--- config/settings.py
+++ config/settings.py
@@ -64,7 +64,12 @@ SESSION_ENGINE = "django.contrib.sessions.backends.db"
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# Vercel serves static files via StaticFilesHandler (DEBUG=True); Render via whitenoise middleware after collectstatic
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"
